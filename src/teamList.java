692fab002294fb0ad3748c95af7627eaf269ce05
Daniel Wivagg <dpwivagg@gmail.com>
2017-10-26 19:16:35 -0400

    Add Dan's name for project

--- src/teamList.java
+++ src/teamList.java
@@ -1,5 +1,6 @@
 public class teamList {
     public static void main(String[] args) {
-        System.println.out("Kenneth_Quartuccio");
+	System.println.out("Daniel Wivagg");       
+	System.println.out("Kenneth_Quartuccio");
     }
 }
