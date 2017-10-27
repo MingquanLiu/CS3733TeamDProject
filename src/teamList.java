22142a0c5574d1e9c055bf629a4ec17b989e612e
HaiauDuong <duong.h.pgd@gmail.com>
2017-10-26 20:05:06 -0400

    Added Haiau Duong

--- src/teamList.java
+++ src/teamList.java
@@ -6,7 +6,8 @@ public class teamList {
         System.out.println("Yufei_Gao");
         System.out.println("Ben Newmark");
         System.out.println("Michael Abadjiev");
-	System.out.println("Mingquan Liu");
-	System.out.println("Shreeja Bhattacharjee");
+	    System.out.println("Mingquan Liu");
+	    System.out.println("Shreeja Bhattacharjee");
+        System.println.out("Haiau Duong");
     }
 }
