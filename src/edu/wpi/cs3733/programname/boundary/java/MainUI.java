10749fed506fee4973fec92fc400d0445fb47c60
michael <michaelabadjiev@gmail.com>
2017-11-09 18:19:27 -0500

    added UI fxml

--- src/edu/wpi/cs3733/programname/boundary/java/MainUI.java
+++ src/edu/wpi/cs3733/programname/boundary/java/MainUI.java
@@ -1,5 +1,11 @@
 package edu.wpi.cs3733.programname.boundary.java;
 
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+
 public class MainUI {
+    @FXML
+    private Button go;
+
 
 }
