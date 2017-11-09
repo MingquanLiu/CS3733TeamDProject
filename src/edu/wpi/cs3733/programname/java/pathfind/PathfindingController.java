971541aeacb3670018b1508e15b431c66e198e28
Unknown <dswenarton@gmail.com>
2017-11-09 14:53:28 -0500

    More package structure

--- src/edu/wpi/cs3733/programname/java/pathfind/PathfindingController.java
+++
@@ -1,4 +0,0 @@
-package edu.wpi.cs3733.programname.java.pathfind;
-
-public class PathfindingController {
-}
---
+++ src/edu/wpi/cs3733/programname/pathfind/PathfindingController.java
@@ -0,0 +1,4 @@
+package edu.wpi.cs3733.programname.pathfind;
+
+public class PathfindingController {
+}
