15b52da11f0dc2740a652d4b7bfa4c0f15a487a6
Yufei Gao <ygao6@wpi.edu>
2017-11-10 14:09:44 -0500

    Employee class
    fields and getter method added

--- src/edu/wpi/cs3733/programname/servicerequest/entity/Employee.java
+++ src/edu/wpi/cs3733/programname/servicerequest/entity/Employee.java
@@ -1,5 +1,20 @@
 package edu.wpi.cs3733.programname.servicerequest.entity;
 
 public class Employee {
+    private String name;
+    private int id;
+    private boolean sysAdmin;
+
+    // constructor of Employee
+    public Employee(String name, int id, boolean sysAdmin){
+        this.name = name;
+        this.id = id;
+        this.sysAdmin = sysAdmin;
+    }
+
+    // getter of sysAdmin
+    public boolean getSysAdmin(){
+        return this.sysAdmin;
+    }
 
 }
