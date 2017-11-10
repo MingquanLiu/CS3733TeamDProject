06069bac8ae6b427640777ca0fa051d599ba2c87
Yufei Gao <ygao6@wpi.edu>
2017-11-10 18:05:52 -0500

    methods created for DatabaseModificationController

--- src/edu/wpi/cs3733/programname/database/DatabaseModificationController.java
+++ src/edu/wpi/cs3733/programname/database/DatabaseModificationController.java
@@ -1,4 +1,28 @@
 package edu.wpi.cs3733.programname.java.database;
 
 public class DatabaseModificationController {
+
+    public DatabaseModificationController(){}
+
+    // All of the methods' return types are String, just for now
+    public String addNode(NodeData node){
+
+    }
+
+    public String editNode(NodeData node){
+
+    }
+
+    public String deleteNode(NodeData node){
+
+    }
+
+    public String addEdge(String node1Id, String node2Id){
+
+    }
+
+    public String deleteEdge(Edge edge){
+
+    }
+
 }
