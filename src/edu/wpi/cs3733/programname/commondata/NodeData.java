c9d885f76f13326e32edea0aa0e6ec8a94a86fe8
kaquartuccio <kaquartuccio@wpi.edu>
2017-11-10 13:25:39 -0500

    Filled out method descriptions

--- src/edu/wpi/cs3733/programname/commondata/NodeData.java
+++ src/edu/wpi/cs3733/programname/commondata/NodeData.java
@@ -3,7 +3,9 @@ package edu.wpi.cs3733.programname.commondata;
 public class NodeData {
 
 
+    // returns the X coordinate of the node
     public int getX() {}
 
+    // returns the Y coordinate of the node
     public int getY() {}
 }
