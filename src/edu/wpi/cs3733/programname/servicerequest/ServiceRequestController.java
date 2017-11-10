4d07296a82a74b0472235cca6176c9595dceeda5
Yufei Gao <ygao6@wpi.edu>
2017-11-10 14:55:58 -0500

    Added method in ServiceRequestController
    Added method createServiceRequest in class ServiceRequestController.

--- src/edu/wpi/cs3733/programname/servicerequest/ServiceRequestController.java
+++ src/edu/wpi/cs3733/programname/servicerequest/ServiceRequestController.java
@@ -6,4 +6,16 @@ public class ServiceRequestController {
      */
     public ServiceRequestController(){ }
 
+    /**
+     * create a new service request with given sender and type
+     * @param sender the employee who sends the service request
+     * @param type the type of the service request
+     */
+    public createServiceRequest(Employee sender, RequestType type){
+        // get the local time and date
+        LocalDateTime date = new LocalDateTime();
+        date = LocalDateTime.now();
+        ServiceRequest newRequest = new ServiceRequest(sender, date, type);
+    }
+
 }
