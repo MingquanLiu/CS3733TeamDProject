4cc945def9940ddeea6a6128dd422bbf58d37e1f
Shreeja <sbhattacharjee@wpi.edu>
2017-11-10 14:29:20 -0400

    ServiceRequest class
    fields, constructor, Javadoc for constructor all added

--- src/edu/wpi/cs3733/programname/servicerequest/entity/ServiceRequest.java
+++ src/edu/wpi/cs3733/programname/servicerequest/entity/ServiceRequest.java
@@ -1,5 +1,23 @@
 package edu.wpi.cs3733.programname.servicerequest.entity;
 
 public class ServiceRequest {
+    private Employee sender;
+    private LocalDateTime date;
+    private Enumeration<RequestType> type;
+
+
+    /**
+     * constructor for class ServiceRequest
+     * @param sender the employee who sent the service request
+     * @param date the date AND time of when service request was sent
+     * @param type the type of service request
+     */
+    public ServiceRequest(Employee sender, LocalDateTime date, Enumeration<RequestType> type) {
+        this.sender = sender;
+        this.date = date;
+        this.type = type;
+    }
+
+
 
 }
