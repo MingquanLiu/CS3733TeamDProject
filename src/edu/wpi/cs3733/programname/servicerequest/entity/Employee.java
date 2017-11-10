66621865fadd1a463fc1ded03208eab93c779777
Yufei Gao <ygao6@wpi.edu>
2017-11-10 14:21:44 -0500

    comment for employee class
    Added comment for employee class

--- src/edu/wpi/cs3733/programname/servicerequest/entity/Employee.java
+++ src/edu/wpi/cs3733/programname/servicerequest/entity/Employee.java
@@ -5,14 +5,22 @@ public class Employee {
     private int id;
     private boolean sysAdmin;
 
-    // constructor of Employee
+    /**
+     * constructor for class Employee
+     * @param name the name of the employee
+     * @param id the id of the employee
+     * @param sysAdmin whether the employee is a system admin
+     */
     public Employee(String name, int id, boolean sysAdmin){
         this.name = name;
         this.id = id;
         this.sysAdmin = sysAdmin;
     }
 
-    // getter of sysAdmin
+    /**
+     * getter for sysAdmin
+     * @return whether the employee is a system admin
+     */
     public boolean getSysAdmin(){
         return this.sysAdmin;
     }
