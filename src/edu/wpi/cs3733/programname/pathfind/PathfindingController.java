f0adabf3f98d71a8e84cb2e99ac1482a087695ec
Ben Newmark <brnewmark@wpi.edu>
2017-11-11 15:31:05 -0500

    added more code, merged some of AStar into this to work with it a little bit

--- src/edu/wpi/cs3733/programname/pathfind/PathfindingController.java
+++ src/edu/wpi/cs3733/programname/pathfind/PathfindingController.java
@@ -1,4 +1,23 @@
 package edu.wpi.cs3733.programname.pathfind;
 
+import edu.wpi.cs3733.programname.ManageController;
+import edu.wpi.cs3733.programname.commondata.Edge;
+import edu.wpi.cs3733.programname.pathfind.entity.AStar;
+import edu.wpi.cs3733.programname.pathfind.PathStrategies.StandardPath;
+
+import java.util.List;
+
 public class PathfindingController {
+
+    public List<Edge> initializePathfind(){
+        ManageController manager = new ManageController();
+        List<Edge> allEdges = manager.getAllEdgeData()
+                //something about getEdges()
+        return null; //return the getedges
+    }
+
+    private List<Edge> initializeAstar(List<Edge> edges){
+        AStar.getPath(PathStrategyIF.getEdges());
+        return null;
+    }
 }
