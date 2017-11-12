be13d51c1a7a2074eb7168cf342d5004678968e3
Shreeja <sbhattacharjee@wpi.edu>
2017-11-12 16:20:21 -0400

    DBModConTest, added more tests
    In the DBModConTest, testssss for days, none of which work :(

--- src/edu/wpi/cs3733/test/DBModConTest.java
+++ src/edu/wpi/cs3733/test/DBModConTest.java
@@ -7,11 +7,23 @@ import static org.junit.Assert.assertEquals;
 
 public class DBModConTest {
 
+
+    Coordinate aBathroomCoord = new Coordinate(4125, 625);
+    NodeData aBathroom = new NodeData ("DREST00102", aBathroomCoord, "REST","Restroom B elevator Floor 2", "Restroom B");
+    DatabaseModficationController theDBModControl = new DatabaseModificationController ();
+
     @Test
-    public void addNode1(){
-        Coordinate aBathroomCoord = new Coordinate(4125, 625);
-        NodeData aBathroom = new NodeData ("DREST00102", aBathroomCoord, "REST","Restroom B elevator Floor 2", "Restroom B");
-        DatabaseModficationController theDBModControl = new DatabaseModificationController ();
+    public String addNode1(){
         assertEquals("insert into Node values (DREST00102, 4125, 625, REST, Restroom B elevator Floor 2, Restroom B)", theDBModControl.addNode(aBathroom));
+    }
+
+    @Test
+    public String addNode2(){
+        assertEquals("Error", theDBModControl.addNode(aBathroom));
+    }
+
+    @Test
+    public String editNode1(){
+        assertEquals("")
     }
 }
