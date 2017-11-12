d65114c71d0369884e2dc70ef31ccc124a164030
Daniel Wivagg <dpwivagg@gmail.com>
2017-11-12 17:24:27 -0500

    Start to implement astar method

--- src/edu/wpi/cs3733/programname/pathfind/entity/AStar.java
+++ src/edu/wpi/cs3733/programname/pathfind/entity/AStar.java
@@ -10,7 +10,18 @@ public class AStar {
         this.frontier = frontier;
     }
 
-    public LinkedList<StarNode> pathFind() {}
+    public LinkedList<StarNode> pathFind(StarNode start, StarNode goal) {
+        Serial.out.println("Starting A*");
+
+        while(!frontier.isEmpty()) {
+            StarNode current = frontier.getFirst();
+            if(current.getX() == goal.getX() && current.getY() == goal.getY()) {
+                Serial.out.println("At target!");
+
+            }
+            
+        }
+    }
 
     public int actionCost(StarNode node) {}
 
--- src/edu/wpi/cs3733/programname/pathfind/entity/StarNode.java
+++ src/edu/wpi/cs3733/programname/pathfind/entity/StarNode.java
@@ -1,5 +1,7 @@
 package edu.wpi.cs3733.programname.pathfind.entity;
 
+import java.util.LinkedList;
+
 public class StarNode extends edu.wpi.cs3733.programname.commondata.NodeData {
     int f, g, h;
 
@@ -7,5 +9,9 @@ public class StarNode extends edu.wpi.cs3733.programname.commondata.NodeData {
         this.f = f;
         this.g = g;
         this.h = h;
+    }
+
+    public LinkedList<StarNode> getNeighbors() {
+        return null;
     }
 }
