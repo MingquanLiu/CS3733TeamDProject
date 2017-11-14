6161296c9ec993a563b533797cab4d818abae87f
Ben Newmark <brnewmark@wpi.edu>
2017-11-14 17:34:59 -0500

    Added documentation for methods and constructors

--- src/edu/wpi/cs3733/programname/pathfind/PathStrategies/StandardPath.java
+++ src/edu/wpi/cs3733/programname/pathfind/PathStrategies/StandardPath.java
@@ -1,15 +1,23 @@
 package edu.wpi.cs3733.programname.pathfind.PathStrategies;
 
+import edu.wpi.cs3733.programname.commondata.Edge;
 import edu.wpi.cs3733.programname.pathfind.PathStrategyIF;
-
 import java.util.List;
 
 public class StandardPath implements PathStrategyIF{
+    /**
+     * for Iteration 2, when we'll be adding restriction types
+     * @return - returns a placeholder null value
+     */
     @Override
     public List<Edge> getEdges() {
         return null;
     }
 
+    /**
+     * A placeholder for Iteration 2
+     * This makes StandardPath a type of PathStrategy
+     */
     public void PathStrategy(){}
 
 
