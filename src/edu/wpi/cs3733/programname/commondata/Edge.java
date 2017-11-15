fec195a3c911127e954cc1ce5257a274fde24680
Shreeja <sbhattacharjee@wpi.edu>
2017-11-15 11:47:35 -0400

    comments for Edge class
    added javadoc documentation for edge class

--- src/edu/wpi/cs3733/programname/commondata/Edge.java
+++ src/edu/wpi/cs3733/programname/commondata/Edge.java
@@ -7,7 +7,7 @@ public class Edge {
     private String edgeId;
 
     /**
-     * Represents the connection between two Nodes
+     * constructor for Coordinate-Represents the connection between two Nodes
      * @param first - the starting point of a given Edge
      * @param second - the ending point of a given Edge
      * @param edgeId - the unique identifier for a given Edge
@@ -18,30 +18,59 @@ public class Edge {
         this.edgeId = edgeId;
     }
 
+    /**
+     * getter for nodeID of first node of an edge
+     * @return the nodeID of the first ID
+     */
     public String getFirstNodeId() {
         return firstNodeId;
     }
 
+    /**
+     * setter for nodeID of first node of an edge
+     * @param firstNodeId the nodeID of the first ID
+     */
     public void setFirstNodeId(String firstNodeId) {
         this.firstNodeId = firstNodeId;
     }
 
+    /**
+     * getter for nodeID of second node of an edge
+     * @return the nodeID of the second node
+     */
     public String getSecondNodeId() {
         return secondNodeId;
     }
 
+    /**
+     * setter for nodeID of a second node of an edge
+     * @param secondNodeId the nodeID of the second node
+     */
     public void setSecondNodeId(String secondNodeId) {
         this.secondNodeId = secondNodeId;
     }
 
+    /**
+     * getter for ID of an edge
+     * @return the ID of the edge
+     */
     public String getEdgeId() {
         return edgeId;
     }
 
+    /**
+     * setter for ID of an edge
+     * @param edgeId the ID of the edge
+     */
     public void setEdgeId(String edgeId) {
         this.edgeId = edgeId;
     }
 
+    /**
+     * checks if generic object is equal
+     * @param o any generic object
+     * @return true if objects equal, else false
+     */
     @Override
     public boolean equals(Object o) {
         if (this == o) return true;
@@ -54,6 +83,10 @@ public class Edge {
         return edgeId != null ? edgeId.equals(edge.edgeId) : edge.edgeId == null;
     }
 
+    /**
+     * sets the hascode using nodeIDs
+     * @return the hashcode
+     */
     @Override
     public int hashCode() {
         int result = firstNodeId != null ? firstNodeId.hashCode() : 0;
