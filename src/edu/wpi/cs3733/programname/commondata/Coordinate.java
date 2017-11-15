bdeb7df6d9d5e542998cdb1e1a3c1a33cc7a05b2
Shreeja <sbhattacharjee@wpi.edu>
2017-11-15 11:38:03 -0400

    documentation for coordiante class
    javadoc for coordinate class

--- src/edu/wpi/cs3733/programname/commondata/Coordinate.java
+++ src/edu/wpi/cs3733/programname/commondata/Coordinate.java
@@ -5,23 +5,49 @@ public class Coordinate {
     private int x;
     private int y;
 
+    /**
+     * constructor for Coordinate class
+     * @param x the x coordinate
+     * @param y the y coordinate
+     */
     public Coordinate(int x, int y) {
         this.x = x;
         this.y = y;
     }
 
+    /**
+     * getter for x coordinate
+     * @return the x coordinate
+     */
     public int getX() {
         return x;
     }
 
+    /**
+     * getter for y coordinate
+     * @return the y coordinate
+     */
     public int getY() {
         return y;
     }
 
+    /**
+     * setter for x coordinate
+     * @param x the coordinate to be set
+     */
     public void setX(int x) { this.x = x; }
 
+    /**
+     * setter for y coordinate
+     * @param y the coordinate to be set
+     */
     public void setY(int y) { this.y = y; }
 
+    /**
+     * checks if generic object is equal
+     * @param o any object
+     * @return true if objects equal, else false
+     */
     @Override
     public boolean equals(Object o) {
         if (this == o) return true;
@@ -33,6 +59,10 @@ public class Coordinate {
         return y == that.y;
     }
 
+    /**
+     * converts item to hash code using the x and y coordinate
+     * @return the hashcode
+     */
     @Override
     public int hashCode() {
         int result = x;
