8a448511bd3a557b2f42384271831f9e2972c9ee
Daniel Wivagg <dpwivagg@gmail.com>
2017-11-19 12:07:20 -0500

    Rudimentary test is working but needs refinement

--- src/edu/wpi/cs3733/programname/pathfind/entity/TextDirections.java
+++ src/edu/wpi/cs3733/programname/pathfind/entity/TextDirections.java
@@ -2,6 +2,7 @@ package edu.wpi.cs3733.programname.pathfind.entity;
 
 import edu.wpi.cs3733.programname.commondata.NodeData;
 
+import java.util.Collections;
 import java.util.HashMap;
 import java.util.List;
 
@@ -11,45 +12,44 @@ public class TextDirections {
 
     public TextDirections(List<NodeData> nodeList) {
         this.nodeList = nodeList;
+        Collections.reverse(this.nodeList);
         produceText();
     }
 
     private void produceText() {
-        NodeData lastNode = null;
-        for(NodeData node: this.nodeList) {
+        // Gotta start somewhere
+        directions = "Begin at " + nodeList.get(0).getLongName();
+        NodeData lastNode;
+        NodeData node;
+
+        for(int i = 1; i < nodeList.size() - 1; i++) {
+            node = nodeList.get(i);
+            lastNode = nodeList.get(i-1);
             String type = node.getType();
             String name = node.getLongName();
             String face;
 
-            if(lastNode == null) {
-                // We need to start somewhere!
-                directions = "Begin at " + name;
+            double directionChange = Math.atan2((lastNode.getY()-node.getY()),(lastNode.getX()-node.getX()));
+            if(directionChange < 90 && directionChange > 50) face = "left";
+            else if(directionChange > -90 && directionChange < -50) face = "right";
+            else face = "straight";
+            switch (type) {
+                case "ELEV":
+                    directions += "\nGet on " + name;
+                    break;
+                case "STAI":
+                    directions += "\nEnter " + name;
+                    break;
+                case "HALL":
+                    if(!lastNode.getType().equals("HALL"))
+                        directions += "\nGo " + face + " down the hall";
+                    break;
+                default:
+                    directions += "\nContinue past " + name;
+                    break;
             }
-
-            else {
-                double directionChange = Math.atan2((lastNode.getY()-node.getY()),(lastNode.getX()-node.getX()));
-                if(directionChange < 90 && directionChange > 50) face = "left";
-                else if(directionChange > -90 && directionChange < -50) face = "right";
-                else face = "straight";
-                switch (type) {
-                    case "ELEV":
-                        directions += "\nGet on " + name;
-                        break;
-                    case "STAI":
-                        directions += "\nEnter " + name;
-                        break;
-                    case "HALL":
-                        if(!lastNode.getType().equals("HALL"))
-                            directions += "\nGo " + face + " down the hall";
-                        break;
-                    default:
-                        directions += "\nContinue past " + name;
-                        break;
-                }
-            }
-
-            lastNode = node;
         }
+        directions += "\nYou have arrived at " + nodeList.get(nodeList.size()-1).getLongName();
     }
 
 
