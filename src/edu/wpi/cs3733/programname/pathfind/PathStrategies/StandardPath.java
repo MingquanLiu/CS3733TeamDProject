6cc733e737d6e9ab07fe622199de9ad25faa783c
Treksh Marwaha <trekshcool@gmail.com>
2017-11-19 16:57:13 -0500

    Implemented Restrictions for pathfinding NOTE: deapth and breath search are currently disabled

--- src/edu/wpi/cs3733/programname/pathfind/PathStrategies/StandardPath.java
+++ src/edu/wpi/cs3733/programname/pathfind/PathStrategies/StandardPath.java
@@ -2,6 +2,8 @@ package edu.wpi.cs3733.programname.pathfind.PathStrategies;
 
 import edu.wpi.cs3733.programname.commondata.Edge;
 import edu.wpi.cs3733.programname.pathfind.PathStrategyIF;
+
+import java.util.LinkedList;
 import java.util.List;
 
 public class StandardPath implements PathStrategyIF{
@@ -10,8 +12,8 @@ public class StandardPath implements PathStrategyIF{
      * @return - returns a placeholder null value
      */
     @Override
-    public List<Edge> getEdges() {
-        return null;
+    public LinkedList<Edge> getEdges(LinkedList<Edge> allEdges) {
+        return allEdges;
     }
 
     /**
--- src/edu/wpi/cs3733/programname/pathfind/PathfindingController.java
+++ src/edu/wpi/cs3733/programname/pathfind/PathfindingController.java
@@ -4,6 +4,7 @@ import edu.wpi.cs3733.programname.ManageController;
 import edu.wpi.cs3733.programname.commondata.Coordinate;
 import edu.wpi.cs3733.programname.commondata.Edge;
 import edu.wpi.cs3733.programname.commondata.NodeData;
+import edu.wpi.cs3733.programname.pathfind.PathStrategies.HandicappedPath;
 import edu.wpi.cs3733.programname.pathfind.entity.AStar;
 import edu.wpi.cs3733.programname.pathfind.PathStrategies.StandardPath;
 
@@ -13,23 +14,46 @@ import java.util.LinkedList;
 import java.util.List;
 
 public class PathfindingController {
+
+
     /**
      * Takes in the starting and ending locations, and calls AStar to find the path between them
+     *
      * @param startNode - the starting location for the path
-     * @param endNode - the end node, aka the destination, for the path
+     * @param endNode   - the end node, aka the destination, for the path
      * @return - result is the list of edges efficiently connecting startNode to endNode
      */
-    public List<NodeData> initializePathfind(LinkedList<NodeData> allNodes,LinkedList<Edge> allEdges,String startNode, String endNode){
-        ManageController manager = new ManageController();
+    public List<NodeData> initializePathfind(LinkedList<NodeData> allNodes, LinkedList<Edge> allEdges, String startNode, String endNode, LinkedList<Edge.Restriction> restrictions) {
+        // ManageController manager = new ManageController();
         // List<Edge> allEdges = manager.getAllEdgeData();
-                //something about getEdges()
+        //something about getEdges()
+        LinkedList<Edge> currentList = allEdges;
+        if(restrictions.contains(Edge.Restriction.GENERAL))
+        {
+            StandardPath standardPath = new StandardPath();
+            currentList = standardPath.getEdges(currentList);
+        }
+        if(restrictions.contains(Edge.Restriction.HANDICAPPED))
+        {
+            HandicappedPath handicappedPath = new HandicappedPath();
+            currentList = handicappedPath.getEdges(currentList);
+        }
+        if(restrictions.contains(Edge.Restriction.ASTAR))
+        {
+            AStar newAstar = new AStar(allNodes, currentList, startNode, endNode);
+            return newAstar.getFinalList();
+        }
+        if(restrictions.contains(Edge.Restriction.DEPTH))
+        {
+        return null;
+        }
+        if(restrictions.contains(Edge.Restriction.BREADTH))
+        {
+        return null;
+        }
+        else return null;
 
-        AStar newAstar = new AStar(allNodes,allEdges, startNode, endNode);
-        return newAstar.getFinalList();
+
+
     }
-
-    // getPath needs to take in the starting node and ending node!!! its currently broken.
-    // How do we take in the starting and ending node
-
-
 }
