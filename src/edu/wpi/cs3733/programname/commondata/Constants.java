552fae2d4ca04354699a433f709427c7946606bd
Unknown <dswenarton@gmail.com>
2017-11-19 16:40:06 -0500

    Changed enum to static constants

--- src/edu/wpi/cs3733/programname/commondata/Constants.java
+++ src/edu/wpi/cs3733/programname/commondata/Constants.java
@@ -2,9 +2,7 @@ package edu.wpi.cs3733.programname.commondata;
 
 public class Constants {
 
-    public enum RequestType {
-        INTERPRETER,
-        TRANSPORT,
-        MAINTENANCE
-    }
+    public static String INTERPRETER_REQUEST = "interpreter";
+    public static String MAINTENANCE_REQUEST = "maintenance";
+    public static String TRANSPORTATION_REQUEST = "transportation";
 }
