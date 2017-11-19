514a81e680a1ef078696f28091b3cc45d833121e
Haiau Duong <duong.h.pgd@gmail.com>
2017-11-19 02:05:57 -0500

    Added StaffTitleData with constructor, getters, and setters

---
+++ src/edu/wpi/cs3733/programname/commondata/StaffTitleData.java
@@ -0,0 +1,41 @@
+package edu.wpi.cs3733.programname.commondata;
+
+public class StaffTitleData {
+    String accountName;
+    String accountPassword;
+    String firstName;
+    String middleInitial;
+    String lastName;
+
+    public StaffTitleData (String accountName, String accountPassword, String firstName, String middleInitial, String lastName) {
+        this.accountName = accountName;
+        this.accountPassword = accountPassword;
+        this.firstName = firstName;
+        this.middleInitial = middleInitial;
+        this.lastName = lastName;
+    }
+
+
+    public String getAccountName() {return this.accountName;}
+
+    public void setAccountName(String accountName) {this.accountName = accountName;}
+
+    public String getAccountPassword() {return this.accountPassword;}
+
+    public void setAccountPassword(String accountPassword) {this.accountPassword = accountPassword;}
+
+    public String getFirstName() {return this.firstName;}
+
+    public void setFirstName(String firstName) {this.firstName = firstName;}
+
+    public String getMiddleInitial() {return this.middleInitial;}
+
+    public void setMiddleInitial(String middleInitial) {this.middleInitial = middleInitial;}
+
+    public String getLastName() {return this.lastName;}
+
+    public void setLastName(String lastName) {this.lastName = lastName;}
+
+
+
+}
