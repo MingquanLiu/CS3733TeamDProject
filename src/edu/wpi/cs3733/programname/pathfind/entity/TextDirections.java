6e2a5632ea70a8cfd4294d9228cc24d279199e5b
dpwivagg <dpwivagg@gmail.com>
2017-11-18 20:26:46 -0500

    Begin implementing text directions

--- src/edu/wpi/cs3733/programname/pathfind/entity/TextDirections.java
+++ src/edu/wpi/cs3733/programname/pathfind/entity/TextDirections.java
@@ -2,6 +2,7 @@ package edu.wpi.cs3733.programname.pathfind.entity;
 
 import edu.wpi.cs3733.programname.commondata.NodeData;
 
+import java.util.HashMap;
 import java.util.List;
 
 public class TextDirections {
@@ -13,7 +14,12 @@ public class TextDirections {
     }
 
     private void produceText() {
-        
+        for(NodeData node: this.nodeList) {
+            String type = node.getType();
+            String name = node.getLongName();
+
+            directions += "\nGo to " + name;
+        }
     }
 
 
