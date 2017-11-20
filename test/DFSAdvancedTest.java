b85d3e1e12e2b7378ccec8c529a55110f7755d80
dpwivagg <dpwivagg@gmail.com>
2017-11-19 20:38:55 -0500

    Fix all edges in test files

--- test/DFSAdvancedTest.java
+++ test/DFSAdvancedTest.java
@@ -22,21 +22,21 @@ public class DFSAdvancedTest {
     NodeData node7 = new NodeData("7", new Coordinate(2, 5), "2","Basic", "Hallway Three", "H3");
     NodeData node8 = new NodeData("8", new Coordinate(4, 5), "2","Basic", "Connector One", "C1");
 
-    Edge edge1 = new Edge("1", "2", "E1");
-    Edge edge2 = new Edge("1", "4", "E2");
-    Edge edge3 = new Edge("1", "5", "E3");
-    Edge edge4 = new Edge("2", "4", "E4");
-    Edge edge5 = new Edge("2", "5", "E5");
-    Edge edge6 = new Edge("2", "3", "E6");
-    Edge edge7 = new Edge("4", "3", "E7");
-    Edge edge8 = new Edge("3", "5", "E8");
-    Edge edge9 = new Edge("4", "7", "E6");
-    Edge edge10 = new Edge("7", "3", "E7");
-    Edge edge11 = new Edge("3", "8", "E8");
-    Edge edge12 = new Edge("8", "5", "E8");
-    Edge edge13 = new Edge("6", "3", "E6");
-    Edge edge14 = new Edge("8", "6", "E7");
-    Edge edge15 = new Edge("7", "6", "E8");
+    Edge edge1 = new Edge("1", "2", "E1", null);
+    Edge edge2 = new Edge("1", "4", "E2", null);
+    Edge edge3 = new Edge("1", "5", "E3", null);
+    Edge edge4 = new Edge("2", "4", "E4", null);
+    Edge edge5 = new Edge("2", "5", "E5", null);
+    Edge edge6 = new Edge("2", "3", "E6", null);
+    Edge edge7 = new Edge("4", "3", "E7", null);
+    Edge edge8 = new Edge("3", "5", "E8", null);
+    Edge edge9 = new Edge("4", "7", "E6", null);
+    Edge edge10 = new Edge("7", "3", "E7", null);
+    Edge edge11 = new Edge("3", "8", "E8", null);
+    Edge edge12 = new Edge("8", "5", "E8", null);
+    Edge edge13 = new Edge("6", "3", "E6", null);
+    Edge edge14 = new Edge("8", "6", "E7", null);
+    Edge edge15 = new Edge("7", "6", "E8", null);
 
     LinkedList<NodeData> allNodes = new LinkedList<>(Arrays.asList(node1, node2, node3, node4, node5, node6, node7, node8));
     LinkedList<Edge> allEdges = new LinkedList<>(Arrays.asList(edge1, edge2, edge3, edge4, edge5, edge6, edge7, edge8,
