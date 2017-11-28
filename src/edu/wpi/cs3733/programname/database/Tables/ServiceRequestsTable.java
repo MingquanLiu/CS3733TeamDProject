2e570b22a63c138d7e6856de1c41f89a6ec96ceb
Haiau Duong <duong.h.pgd@gmail.com>
2017-11-27 19:07:50 -0500

    fixed constraint issues with nodes issue

--- src/edu/wpi/cs3733/programname/database/Tables/ServiceRequestsTable.java
+++ src/edu/wpi/cs3733/programname/database/Tables/ServiceRequestsTable.java
@@ -14,8 +14,8 @@ public class ServiceRequestsTable {
                     "serviceType VARCHAR(25), status VARCHAR(10), description VARCHAR(500), requestTime VARCHAR(30), " +
                     "handleTime VARCHAR(30), completionTime VARCHAR(30)," +
                     "CONSTRAINT ServiceRequests PRIMARY KEY (serviceID)," +
-                    "CONSTRAINT ServiceRequests_FK1 FOREIGN KEY (sender) REFERENCES Employees (userName) ON DELETE SET NULL," +
-                    "CONSTRAINT ServiceRequests_FK2 FOREIGN KEY (receiver) REFERENCES Employees (userName) ON DELETE SET NULL," +
+                    "CONSTRAINT ServiceRequests_FK1 FOREIGN KEY (sender) REFERENCES Employees (userName)," +
+                    "CONSTRAINT ServiceRequests_FK2 FOREIGN KEY (receiver) REFERENCES Employees (userName)," +
                     "CONSTRAINT ServiceRequests_FK3 FOREIGN KEY (node1ID) REFERENCES Nodes (nodeID) ON DELETE CASCADE," +
                     "CONSTRAINT ServiceRequests_FK4 FOREIGN KEY (node2ID) REFERENCES Nodes (nodeID) ON DELETE CASCADE)";
 
