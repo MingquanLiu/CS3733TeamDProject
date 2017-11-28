1e1abe39cd18b6fa4c9e6225d679c2a4ccb6335a
Haiau Duong <duong.h.pgd@gmail.com>
2017-11-27 19:03:24 -0500

    fixed constraint issues with nodes issue

--- src/edu/wpi/cs3733/programname/database/Tables/NodesTable.java
+++ src/edu/wpi/cs3733/programname/database/Tables/NodesTable.java
@@ -25,7 +25,7 @@ public class NodesTable {
                     "CONSTRAINT Nodes_floor CHECK (floor IN " +
                     "('L2', 'L1', 'G', '1', '2', '3'))," +
                     "CONSTRAINT Nodes_xcoordVal check (xcoord >= 0 AND xcoord <= 5000)," +
-                    "CONSTRAINT Nodes_ycoordVal check (ycoord >= 0 AND ycoord <= 5000))";
+                    "CONSTRAINT Nodes_ycoordVal check (ycoord >= 0 AND ycoord <= 3400))";
 
             conn.execute(newTable);
             System.out.println("\nNodes Table Created\n");
