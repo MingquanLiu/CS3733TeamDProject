982248a1216c50c2ea61ce259221e6347d0f9cd0
Yufei Gao <ygao6@wpi.edu>
2017-11-29 05:03:24 -0500

    Add FAQ_Popup Controller

--- src/edu/wpi/cs3733/programname/Main.java
+++ src/edu/wpi/cs3733/programname/Main.java
@@ -10,7 +10,7 @@ import javafx.scene.Scene;
 import javafx.scene.layout.Pane;
 import javafx.stage.Stage;
 import javafx.stage.StageStyle;
-import org.junit.Test;
+//import org.junit.Test;
 
 import java.io.IOException;
 
