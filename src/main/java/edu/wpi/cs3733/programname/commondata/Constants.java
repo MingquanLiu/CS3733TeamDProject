721fc37516ed04f73370b5ae82091587d2dde53c
Treksh Marwaha <trekshcool@gmail.com>
2017-12-01 18:49:33 -0500

    Trek Test

--- src/main/java/edu/wpi/cs3733/programname/commondata/Constants.java
+++ src/main/java/edu/wpi/cs3733/programname/commondata/Constants.java
@@ -2,14 +2,21 @@ package edu.wpi.cs3733.programname.commondata;
 
 public class Constants {
 
+// This is just a test comment
+
+
+
     public static String INTERPRETER_REQUEST = "interpreter";
     public static String MAINTENANCE_REQUEST = "maintenance";
     public static String TRANSPORTATION_REQUEST = "transportation";
     public static String NONE = "none";
-
+t
     public static String UNASSIGNED_REQUEST = "unhandled";
     public static String ASSIGNED_REQUEST = "handled";
     public static String COMPLETED_REQUEST = "completed";
     public static String REMOVED_REQUEST = "removed";
+
+    public static String TEST_CONSTANT_PLEASE_IGNORE = "test";
+    public static String TEST_CONSTANT_PLEASE_IGNORE_2 = "test";
 
 }
