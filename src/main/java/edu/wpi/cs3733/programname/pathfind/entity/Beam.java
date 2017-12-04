070878f1d4f2782d80d5e2c19861b2cac66145ab
Daniel Wivagg <dpwivagg@gmail.com>
2017-12-04 15:20:49 -0500

    Create skeleton for new algorithms

---
+++ src/main/java/edu/wpi/cs3733/programname/pathfind/entity/Beam.java
@@ -0,0 +1,21 @@
+package edu.wpi.cs3733.programname.pathfind.entity;
+
+import edu.wpi.cs3733.programname.commondata.EdgeData;
+import edu.wpi.cs3733.programname.commondata.NodeData;
+
+import java.util.List;
+
+public class Beam extends PathfindingStrategyTemplate {
+
+    public Beam(List<NodeData> allNodes, List<EdgeData> allEdges, String startID, String goalID) {
+        this.allNodes = allNodes;
+        this.allEdges = allEdges;
+        this.startID = startID;
+        this.goalID = goalID;
+    }
+
+    List<NodeData> pathFind() throws NoPathException {
+
+        return finalList;
+    }
+}
