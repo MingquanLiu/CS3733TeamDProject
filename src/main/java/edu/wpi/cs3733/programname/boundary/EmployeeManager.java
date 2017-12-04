a626e7f42c43977d9b2bb7535576bb9c7af634f6
Daniel Wivagg <dpwivagg@gmail.com>
2017-12-03 19:04:52 -0500

    Fully working employee UI, ready to merge closes #73

--- src/main/java/edu/wpi/cs3733/programname/boundary/EmployeeManager.java
+++ src/main/java/edu/wpi/cs3733/programname/boundary/EmployeeManager.java
@@ -7,8 +7,10 @@ import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
 import javafx.collections.transformation.SortedList;
 import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
 import javafx.fxml.FXML;
 import javafx.scene.control.*;
+import javafx.scene.control.cell.TextFieldTableCell;
 import javafx.scene.input.MouseEvent;
 import javafx.scene.layout.AnchorPane;
 import javafx.stage.Stage;
@@ -86,6 +88,12 @@ public class EmployeeManager {
     private JFXButton remove;
 
     @FXML
+    private JFXButton edit;
+
+    @FXML
+    private JFXButton canceledit;
+
+    @FXML
     private Label dataerror;
 
     @FXML
@@ -94,6 +102,9 @@ public class EmployeeManager {
     @FXML
     private Label changessaved;
 
+    @FXML
+    private Label addlabel;
+
     private ManageController manageController;
 
     private final ObservableList<Employee> data = FXCollections.observableArrayList();
@@ -101,8 +112,12 @@ public class EmployeeManager {
     @FXML
     private SortedList<Employee> sortedEmployee;
 
+
     @FXML
     void addNewEmployee(ActionEvent event) {
+        boolean flag = false;
+        canceledit.setVisible(false);
+        add.setText("Add employee");
         try {
             if (!password.getText().equals(password1.getText())) {
                 passerror.setVisible(true);
@@ -130,8 +145,16 @@ public class EmployeeManager {
                 Employee newuser = new Employee(username, newpassword, firstname, "", lastname, adminAccess,
                         serviceType, email);
 
-                manageController.addEmployee(newuser);
-                changessaved.setVisible(true);
+                for(Employee e: sortedEmployee) {
+                    if(e.getUsername().equals(username)) {
+                        manageController.editEmployee(newuser);
+                        flag = true;
+                    }
+                }
+                if(!flag) {
+                    manageController.addEmployee(newuser);
+                    changessaved.setVisible(true);
+                }
 
                 newusername.clear();
                 newfirstname.clear();
@@ -165,8 +188,49 @@ public class EmployeeManager {
     }
 
     @FXML
+    private void updateRow(MouseEvent event) {
+        this.hideChangesSaved(event);
+        edit.setVisible(true);
+    }
+
+    @FXML
     private void hideChangesSaved(MouseEvent event) {
         changessaved.setVisible(false);
+    }
+
+    @FXML
+    private void showedit(ActionEvent event) {
+        edit.setVisible(false);
+        canceledit.setVisible(true);
+        Employee employee = employeetable.getSelectionModel().getSelectedItem();
+        newusername.setText(employee.getUsername());
+        newfirstname.setText(employee.getFirstName());
+        newlastname.setText(employee.getLastName());
+        newemail.setText(employee.getEmail());
+        password.setText(employee.getPassword());
+        password1.setText(employee.getPassword());
+        admin.setSelected(employee.getSysAdmin());
+        if(employee.getServiceType().equals("transportation")) servicegroup.selectToggle(transportBtn);
+        else if(employee.getServiceType().equals("maintenance")) servicegroup.selectToggle(maintenanceBtn);
+        else servicegroup.selectToggle(interpreterBtn);
+        addlabel.setText("Edit employee " + employee.getUsername());
+        add.setText("Update employee");
+    }
+
+    @FXML
+    private void cancelEdit(ActionEvent event) {
+        canceledit.setVisible(false);
+        employeetable.getSelectionModel().clearSelection();
+        addlabel.setText("Add a new employee");
+        add.setText("Add employee");
+        newusername.clear();
+        newfirstname.clear();
+        newlastname.clear();
+        newemail.clear();
+        password.clear();
+        password1.clear();
+        admin.setSelected(false);
+        servicegroup.selectToggle(null);
     }
 
     @FXML
@@ -175,11 +239,6 @@ public class EmployeeManager {
         Stage stage = (Stage) close.getScene().getWindow();
         changessaved.setVisible(false);
         stage.close();
-    }
-
-    @FXML
-    void updateLastName(ActionEvent event) {
-
     }
 
     public void initManager(ManageController manageController){
