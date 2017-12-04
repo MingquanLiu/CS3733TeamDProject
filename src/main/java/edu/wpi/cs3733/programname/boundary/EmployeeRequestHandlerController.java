9a544988f76c7841de6c52191de267212d6c2d5f
Daniel Wivagg <dpwivagg@gmail.com>
2017-12-03 20:09:45 -0500

    Add skeleton interface and controller

--- src/main/java/edu/wpi/cs3733/programname/boundary/EmployeeRequestHandlerController.java
+++ src/main/java/edu/wpi/cs3733/programname/boundary/EmployeeRequestHandlerController.java
@@ -1,4 +1,43 @@
 package edu.wpi.cs3733.programname.boundary;
 
+import com.jfoenix.controls.JFXButton;
+import com.jfoenix.controls.JFXListView;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Label;
+
 public class EmployeeRequestHandlerController {
+        @FXML
+        private JFXListView<?> listUnassigned;
+
+        @FXML
+        private JFXListView<?> listEmployeeRequests;
+
+        @FXML
+        private Label employeeName;
+
+        @FXML
+        private JFXButton btnAccept;
+
+        @FXML
+        private JFXButton btnComplete;
+
+        @FXML
+        private JFXButton btnRemove;
+
+        @FXML
+        void acceptButtonHandler(ActionEvent event) {
+
+        }
+
+        @FXML
+        void markCompleteButtonHandler(ActionEvent event) {
+
+        }
+
+        @FXML
+        void unhandleRequestButtonHandler(ActionEvent event) {
+
+        }
+
 }
