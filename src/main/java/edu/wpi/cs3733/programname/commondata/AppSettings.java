9d72b7a75223a3b4263e1c474afd64f35cb6fca2
Unknown <dswenarton@gmail.com>
2017-12-04 23:12:28 -0500

    Added Singleton for AppSettings

---
+++ src/main/java/edu/wpi/cs3733/programname/commondata/AppSettings.java
@@ -0,0 +1,46 @@
+package edu.wpi.cs3733.programname.commondata;
+
+import edu.wpi.cs3733.programname.boundary.TestingController;
+import edu.wpi.cs3733.programname.pathfind.PathfindingController;
+import edu.wpi.cs3733.programname.pathfind.PathfindingController.searchType;
+
+public class AppSettings {
+
+    private static AppSettings appSettings = new AppSettings();
+
+    private searchType searchType;
+    private boolean handicapPath;
+    private int mapRatioIndex;
+    private int currentFloor;       //TODO refactor this to Floor type
+
+    private final int INITIAL_MAP_SCALE = 3;
+
+    private AppSettings() {
+        searchType = PathfindingController.searchType.ASTAR;
+        handicapPath = false;
+        mapRatioIndex = INITIAL_MAP_SCALE;
+        currentFloor = 2;
+    }
+
+    public static AppSettings getInstance() {
+        return appSettings;
+    }
+    public searchType getSearchType() {
+        return this.searchType;
+    }
+    public void setSearchType(searchType type) {
+        this.searchType = type;
+    }
+    public boolean isHandicapPath() {
+        return handicapPath;
+    }
+    public void setHandicapPath(boolean handicapPath) {
+        this.handicapPath = handicapPath;
+    }
+    public int getMapRatioIndex() {
+        return mapRatioIndex;
+    }
+    public void setMapRatioIndex(int mapRatioIndex) {
+        this.mapRatioIndex = mapRatioIndex;
+    }
+}
