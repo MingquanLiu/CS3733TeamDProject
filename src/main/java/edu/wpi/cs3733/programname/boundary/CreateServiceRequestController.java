74413b6544373cb3fe9460138189742de80bfa1f
Unknown <dswenarton@gmail.com>
2017-12-12 23:38:50 -0500

    Error messages work on creation ui

--- src/main/java/edu/wpi/cs3733/programname/boundary/CreateServiceRequestController.java
+++ src/main/java/edu/wpi/cs3733/programname/boundary/CreateServiceRequestController.java
@@ -9,6 +9,7 @@ import edu.wpi.cs3733.programname.ManageController;
 import edu.wpi.cs3733.programname.commondata.Constants;
 import edu.wpi.cs3733.programname.commondata.Employee;
 import javafx.fxml.FXML;
+import javafx.scene.control.Alert;
 import javafx.scene.control.Label;
 import javafx.stage.Stage;
 import org.controlsfx.control.textfield.AutoCompletionBinding;
@@ -67,6 +68,7 @@ public class CreateServiceRequestController {
     ManageController manager;
     String submissionType;
     Employee loggedIn;
+    List<String> allLongNames;
 
     private AutoCompletionBinding<String> autoCompletionBindingLocation;
     private AutoCompletionBinding<String> autoCompletionBindingTransportDestination;
@@ -74,7 +76,7 @@ public class CreateServiceRequestController {
     public void initManager(ManageController manage, String requestType, Employee loggedIn) {
         manager = manage;
         this.loggedIn = loggedIn;
-        List<String> allLongNames = manager.fuzzyQueryNodesByLongName("");
+        allLongNames = manager.fuzzyQueryNodesByLongName("");
         autoCompletionBindingLocation = TextFields.bindAutoCompletion(txtSelectLocation, allLongNames);
         autoCompletionBindingTransportDestination = TextFields.bindAutoCompletion(txtTransportDestination,allLongNames);
         if (requestType == Constants.TRANSPORTATION_REQUEST) {
@@ -144,35 +146,95 @@ public class CreateServiceRequestController {
     }
 
     public void submitButtonHandler() {
-        int severity = Integer.parseInt(comboSeverity.getSelectionModel().getSelectedItem().toString());
-        String requestLocationId = manager.queryNodeByLongName(txtSelectLocation.getText()).getNodeID();
+        if (checkSubmission()) {
+            int severity = Integer.parseInt(comboSeverity.getSelectionModel().getSelectedItem().toString());
+            String requestLocationId = manager.queryNodeByLongName(txtSelectLocation.getText()).getNodeID();
 
-        if (submissionType == Constants.INTERPRETER_REQUEST) {
-            String language = comboLanguages.getSelectionModel().getSelectedItem().toString();
-            String reservationTime = timePicker.getValue().toString();
-            manager.createInterpreterRequest(loggedIn.getUsername(),
-                    submissionType,
-                    requestLocationId,
-                    null,
-                    txtDescription.getText(),
-                    severity,
-                    language,
-                    reservationTime);
+            if (submissionType == Constants.INTERPRETER_REQUEST) {
+                String language = comboLanguages.getSelectionModel().getSelectedItem().toString();
+                String reservationTime = timePicker.getValue().toString();
+                manager.createInterpreterRequest(loggedIn.getUsername(),
+                        submissionType,
+                        requestLocationId,
+                        null,
+                        txtDescription.getText(),
+                        severity,
+                        language,
+                        reservationTime);
 
-        } else if (submissionType == Constants.MAINTENANCE_REQUEST) {
-            String maintenanceType = comboMaintenanceType.getSelectionModel().getSelectedItem().toString();
-            manager.createMaintenanceRequest(loggedIn.getUsername(), submissionType, requestLocationId, null,
-                    txtDescription.getText(), severity, maintenanceType);
+            } else if (submissionType == Constants.MAINTENANCE_REQUEST) {
+                String maintenanceType = comboMaintenanceType.getSelectionModel().getSelectedItem().toString();
+                manager.createMaintenanceRequest(loggedIn.getUsername(), submissionType, requestLocationId, null,
+                        txtDescription.getText(), severity, maintenanceType);
 
-        } else if (submissionType == Constants.TRANSPORTATION_REQUEST) {
-            String transportType = comboTransportType.getSelectionModel().getSelectedItem().toString();
-            String reservationTime = timePicker.getValue().toString();
-            String destinationId = manager.queryNodeByLongName(txtTransportDestination.getText()).getNodeID();
-            manager.createTransportationRequest(loggedIn.getUsername(), submissionType, requestLocationId, null,
-                    txtDescription.getText(), severity, transportType, destinationId, reservationTime);
+            } else if (submissionType == Constants.TRANSPORTATION_REQUEST) {
+                String transportType = comboTransportType.getSelectionModel().getSelectedItem().toString();
+                String reservationTime = timePicker.getValue().toString();
+                String destinationId = manager.queryNodeByLongName(txtTransportDestination.getText()).getNodeID();
+                manager.createTransportationRequest(loggedIn.getUsername(), submissionType, requestLocationId, null,
+                        txtDescription.getText(), severity, transportType, destinationId, reservationTime);
+            }
+            Stage stage = (Stage) btnSubmit.getScene().getWindow();
+            stage.close();
         }
-        Stage stage = (Stage) btnSubmit.getScene().getWindow();
-        stage.close();
+    }
+
+    public boolean checkSubmission() {
+        Alert alert = new Alert(Alert.AlertType.ERROR);
+        alert.setTitle("");
+        String capital = submissionType.substring(0,1).toUpperCase() + submissionType.substring(1);
+        alert.setHeaderText("Error creating " + capital + " Request!");
+
+        // checks that a request type has been selected
+        if (comboLanguages.getSelectionModel().isEmpty() && submissionType == Constants.INTERPRETER_REQUEST) {
+            alert.setContentText("Please select a language!");
+            alert.showAndWait();
+            return false;
+        } else if (comboMaintenanceType.getSelectionModel().isEmpty()
+                && submissionType == Constants.MAINTENANCE_REQUEST) {
+            alert.setContentText("Please specify a maintenance type!");
+            alert.showAndWait();
+            return false;
+        } else if (comboTransportType.getSelectionModel().isEmpty()
+                && submissionType == Constants.TRANSPORTATION_REQUEST) {
+            alert.setContentText("Please specify a transportation type!");
+            alert.showAndWait();
+            return false;
+        }
+
+        // checks that a valid location has been entered
+        String requestLocationId = manager.queryNodeByLongName(txtSelectLocation.getText()).getNodeID();
+        if (requestLocationId == null || manager.getNodeData(requestLocationId) == null ||
+                txtSelectLocation.getText() == "") {
+            alert.setContentText("Please enter a valid location!");
+            alert.showAndWait();
+            return false;
+        }
+
+        //checks that a severity has been selected
+        if (comboSeverity.getSelectionModel().isEmpty()) {
+            alert.setContentText("Please specify the severity of your request!");
+            alert.showAndWait();
+            return false;
+        }
+
+        //checks if destination is valid for transport requests
+        String destinationId = manager.queryNodeByLongName(txtTransportDestination.getText()).getNodeID();
+        boolean invalidDest = (destinationId == null || manager.getNodeData(destinationId) == null);
+        if (invalidDest && submissionType == Constants.TRANSPORTATION_REQUEST) {
+            alert.setContentText("Please enter a valid destination for your transport!");
+            alert.showAndWait();
+            return false;
+        }
+
+        //checks if time is valid for relevant request types
+        if (timePicker.getValue() == null && (submissionType == Constants.INTERPRETER_REQUEST
+                || submissionType == Constants.TRANSPORTATION_REQUEST)) {
+            alert.setContentText("Please enter a valid time for your request!");
+            alert.showAndWait();
+            return false;
+        }
+        return true;
     }
 
 
